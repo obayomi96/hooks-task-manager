a1a97343a2b4aa9ecf17aa37a7dda9e90c99b91d
Martins Obayomi <obayomimartins96@gmail.com>
2020-07-17 09:56:18 +0100

    testing library

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import TodoApp from "./TodoApp";
+import {InternetStatus} from "react-internet-status";
 
 function App() {
-  return <TodoApp />;
+  console.log(window.navigator.onLine);
+  return (
+    <div>
+      <InternetStatus bgColor="green" />
+      <TodoApp />
+    </div>
+  );
 }
 
 export default App;
